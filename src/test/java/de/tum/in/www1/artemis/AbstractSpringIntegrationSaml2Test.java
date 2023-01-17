2312a54e82347171b8e2293ca0edd45cfcbb436c
Julian Christl <julian.christl@tum.de>
2023-01-17 10:29:00 +0100

    move authentication to public

--- src/test/java/de/tum/in/www1/artemis/AbstractSpringIntegrationSaml2Test.java
+++ src/test/java/de/tum/in/www1/artemis/AbstractSpringIntegrationSaml2Test.java
@@ -28,7 +28,7 @@ import de.tum.in.www1.artemis.service.connectors.SAML2Service;
 import de.tum.in.www1.artemis.service.connectors.jenkins.JenkinsProgrammingLanguageFeatureService;
 import de.tum.in.www1.artemis.util.DatabaseUtilService;
 import de.tum.in.www1.artemis.util.RequestUtilService;
-import de.tum.in.www1.artemis.web.rest.UserJwtResource;
+import de.tum.in.www1.artemis.web.rest.publicc.UserJwtResource;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 
 /**
