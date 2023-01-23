4c6e22ee8a9d25154266d0556ed35e489bd6bf66
Maximilian Soelch <maximilian.soelch@tum.de>
2023-01-23 20:37:20 +0100

    revert fix participation_deletion

--- src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
@@ -1,4 +1,4 @@
-package de.tum.in.www1.artemis.service;
+    package de.tum.in.www1.artemis.service;
 
 import static de.tum.in.www1.artemis.domain.enumeration.InitializationState.*;
 
@@ -766,7 +766,7 @@ public class ParticipationService {
         // By removing the participation, the ResultListener will ignore this result instead of scheduling a participant score update
         // This is okay here, because we delete the whole participation (no older results will exist for the score)
         resultsToBeDeleted.forEach(participation::removeResult);
-        resultsToBeDeleted.forEach(result -> resultService.deleteResult(result, true));
+        resultsToBeDeleted.forEach(result -> resultService.deleteResult(result, false));
         // Delete all submissions for this participation
         submissions.forEach(submission -> {
             if (submission instanceof ProgrammingSubmission) {
